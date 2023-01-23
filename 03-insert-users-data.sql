218abe5cd37a8eb5e5402930169a6789492147c9
alex <o.lototskyi@gmail.com>
2023-01-23 19:16:17 +0200

    NUMERIC, FLOAT data types

---
+++ 03-insert-users-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (yearly_salary, full_name, current_status) 
+VALUES (19000, 'Max Schwarz', 'self-employed');
+INSERT INTO users (yearly_salary, full_name, current_status) 
+VALUES (25000, 'Christine Newman', 'employed');
+INSERT INTO users (yearly_salary, full_name, current_status) 
+VALUES (0, 'Perry Long', 'unemployed');
