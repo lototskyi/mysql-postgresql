b17fca54e5824e1c7056e22b48d099222ec5e202
alex <o.lototskyi@gmail.com>
2023-01-25 16:08:48 +0200

    PRIMARY KEY and AUTO_INCREMENT

---
+++ 17-primary-key.sql
@@ -0,0 +1,32 @@
+DROP TABLE users;
+DROP TABLE employers;
+DROP TABLE conversation;
+
+-- CREATE TYPE employment_status AS ENUM('employed', 'self-employed', 'unemployed'); -- Postgresql ENUM
+
+CREATE TABLE users (
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- Postgresql
+    full_name VARCHAR(300) NOT NULL,
+    yearly_salary INT CHECK (yearly_salary > 0),
+    -- current_status ENUM('employed', 'self-employed', 'unemployed') -- MySQL
+    current_status employment_status -- Postgresql ENUM
+);
+
+CREATE TABLE employers (
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- Postgresql
+    company_name VARCHAR(300) NOT NULL,
+    company_address VARCHAR(300) NOT NULL,
+    yearly_revenue FLOAT CHECK (yearly_revenue > 0),
+    is_hiring BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE conversations (
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- Postgresql
+    user_id INT,
+    employesr_id INT,
+    message TEXT NOT NULL,
+    date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
