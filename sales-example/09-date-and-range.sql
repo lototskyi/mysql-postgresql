775eb36269988f0a23ad7dc93ee37c9b95bafcc6
alex <o.lototskyi@gmail.com>
2023-01-26 13:37:12 +0200

    dates and ranges

---
+++ sales-example/09-date-and-range.sql
@@ -0,0 +1,13 @@
+-- SELECT * FROM sales
+-- WHERE 
+--     date_created > '2022-01-25' AND 
+--     date_created < '2023-05-01';
+
+-- SELECT * FROM sales
+-- WHERE date_created BETWEEN '2022-01-25' AND '2023-05-01';
+
+-- SELECT * FROM sales
+-- WHERE volume > 1000 AND volume < 100000;
+
+-- SELECT * FROM sales
+-- WHERE volume BETWEEN 1000 AND 100000;
