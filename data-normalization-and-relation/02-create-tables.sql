c66aa2c56b0aef733fe1c95195a3fc81ad1aaf17
alex <o.lototskyi@gmail.com>
2023-01-27 21:49:32 +0200

    data normalization, relationships, INNER JOIN

---
+++ data-normalization-and-relation/02-create-tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- Postgres
+    first_name VARCHAR(300) NOT NULL,
+    last_name VARCHAR(300) NOT NULL,
+    email VARCHAR(300) NOT NULL,
+    address_id INT NOT NULL
+);
+
+CREATE TABLE addresses (
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- Postgres
+    street VARCHAR(300) NOT NULL,
+    house_number VARCHAR(50) NOT NULL,
+    city_id INT NOT NULL
+);
+
+CREATE TABLE cities (
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- Postgres
+    name VARCHAR(300) NOT NULL
+);
