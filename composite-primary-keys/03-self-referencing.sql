81ee884af040c3e789788d3aaefc48f9420130c9
alex <o.lototskyi@gmail.com>
2023-01-31 10:46:53 +0200

    Self-Referencing Relationships

---
+++ composite-primary-keys/03-self-referencing.sql
@@ -0,0 +1,6 @@
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY, -- Postgres
+    first_name VARCHAR(300) NOT NULL,
+    last_name VARCHAR(300) NOT NULL,
+    supervisor_id INT REFERENCES employees (id) ON DELETE SET NULL
+);
