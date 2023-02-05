fb8b5c874b3727d7a754e97b9600977bb3d817ce
alex <o.lototskyi@gmail.com>
2023-02-05 14:47:38 +0200

    string functions, CONCAT

---
+++ built-in-query-functions-and-operators/02-string-functions.sql
@@ -0,0 +1,8 @@
+-- SELECT CONCAT(first_name, ' ', last_name)
+-- FROM memberships;
+
+-- SELECT first_name || ' ' || last_name -- postgres
+-- FROM memberships;
+
+SELECT CONCAT('$ ', price)
+FROM memberships;
