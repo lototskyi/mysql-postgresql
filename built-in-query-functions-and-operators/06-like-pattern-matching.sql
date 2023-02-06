61ecb1e1dbe89e7f3a2d60c41b2beed142f7a175
alex <o.lototskyi@gmail.com>
2023-02-06 16:47:45 +0200

    LIKE & Pattern matching

---
+++ built-in-query-functions-and-operators/06-like-pattern-matching.sql
@@ -0,0 +1,12 @@
+-- SELECT first_name LIKE '%u_', first_name
+-- FROM memberships;
+
+-- SELECT first_name
+-- FROM memberships
+-- WHERE first_name like 'J____';
+
+-- Postgres
+-- SELECT first_name ILIKE 'ma%', first_name
+-- FROM memberships;
+
+-- LIKE in MySQL is not case sensitive
